667a905c793b0340054548b70ca807db107d5f77
Michael Stringer Jr <michael.stringer.2.0@gmail.com>
2020-12-17 08:27:52 -0600

    Update shortcut.py

--- shortcut.py
+++ shortcut.py
@@ -19,3 +19,38 @@ for b in range(0,len(board)):
         board_print += board[b]
 board_print += "\n"
 print(board_print)
+
+def check_for_winner(board):
+    
+    # Check for a draw
+    draw_flag = 0
+    for i in range(len(board)):
+        if board[i] is int:
+            draw_flag = 1
+    if draw_flag is 0:
+        return "tie"
+    
+    # Check horizontals
+    if (board[0] == board[1] and board[1] == board[2] and board[0] is not int):
+        return board[0]
+    if (board[3] == board[4] and board[4] == board[5] and board[3] is not int):
+        return board[3]
+    if (board[6] == board[7] and board[7] == board[8] and board[6] is not int):
+        return oard[6]
+    
+    # Check verticals
+    if (board[0] == board[3] and board[3] == board[6] and board[0] is not int):
+        return board[0]
+    if (board[1] == board[4] and board[4] == board[7] and board[1] is not int):
+        return board[1]
+    if (board[2] == board[5] and board[5] == board[8] and board[2] is not int):
+        return board[2]
+    
+    # Check diagonals
+    if (board[0] == board[4] and board[4] == board[8] and board[0] is not int):
+        return board[0]
+    if (board[2] == board[4] and board[4] == board[6] and board[2] is not int):
+        return board[2]
+    
+    #check No one has won
+    return "keep playing"
